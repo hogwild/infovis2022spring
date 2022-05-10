748f02080177a6f80c84ebb5ede54c54cde04f86
hogwild <guxianbinmail@gmail.com>
2022-05-10 16:34:05 +0800

    update

---
+++ team18/precache-manifest.26e93285acfbc1732708507506ab63b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0cd09f00fe42bb2d6fa53f25afccc7",
+    "url": "/infovis2022spring/index.html"
+  },
+  {
+    "revision": "798cdae90a74c89469d0",
+    "url": "/infovis2022spring/static/css/main.dacb0614.chunk.css"
+  },
+  {
+    "revision": "6f50fe570526a32ceca7",
+    "url": "/infovis2022spring/static/js/2.b8c80520.chunk.js"
+  },
+  {
+    "revision": "798cdae90a74c89469d0",
+    "url": "/infovis2022spring/static/js/main.db8e7a0c.chunk.js"
+  },
+  {
+    "revision": "9a362e5a1c4f081f097a",
+    "url": "/infovis2022spring/static/js/runtime~main.947d37f3.js"
+  }
+]);
--- team18/service-worker.js
+++ team18/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6ed8f53a166262cbdb000f483fc1e98.js"
+  "/infovis2022spring/precache-manifest.26e93285acfbc1732708507506ab63b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/infovis2022spring/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

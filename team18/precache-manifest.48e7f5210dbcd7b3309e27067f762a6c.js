ebeaff1ea9eb69dcf22507b74f1f98cdeb983251
hogwild <guxianbinmail@gmail.com>
2022-05-10 15:19:53 +0800

    update

---
+++ team18/precache-manifest.48e7f5210dbcd7b3309e27067f762a6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316ea2e585accfad3050a48239dbb8c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6641baaaa289f6d7b8d3",
+    "url": "/static/css/main.dacb0614.chunk.css"
+  },
+  {
+    "revision": "6f50fe570526a32ceca7",
+    "url": "/static/js/2.b8c80520.chunk.js"
+  },
+  {
+    "revision": "6641baaaa289f6d7b8d3",
+    "url": "/static/js/main.252fc8a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
